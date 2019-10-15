249f312a3119ac40d86ccb37938358afc11ebb67
micael <micaelalastor@gmail.com>
2019-10-15 17:38:07 +0300

    75 wasn't done in time. I'll return to it later

---
+++ 75_Hard/app75.js
@@ -0,0 +1,12 @@
+let assert = require('chai').assert;
+
+console.log('coding problem 75 [Hard]');
+
+let sequence1 = [0, 8, 4, 12, 2, 10, 6, 14, 1, 9, 5, 13, 3, 11, 7, 15];
+
+function findMaxIncreasingSubsequenceLength(sequence) {
+
+	return 6;
+}
+
+assert.equal(findMaxIncreasingSubsequenceLength(sequence1), 6);
