6080366ccb8c61a90731534676790a881672f7ba
micael <micaelalastor@gmail.com>
2019-10-15 23:56:33 +0300

    92 bugfix after adding better test data

--- 92_Hard/app92.js
+++ 92_Hard/app92.js
@@ -3,6 +3,8 @@ let assert = require('chai').assert;
 console.log('coding problem 92 [Hard]');
 
 let courses = {'CSC300': ['CSC100', 'CSC200'], 'CSC200': ['CSC100'], 'CSC100': []};
+let wrong_courses_1 = {'CSC400': ['CSC500'], 'CSC300': ['CSC100', 'CSC200'], 'CSC200': ['CSC100'], 'CSC100': []};
+let wrong_courses_2 = {'CSC400': ['CSC300'], 'CSC300': ['CSC100', 'CSC200'], 'CSC200': ['CSC100', 'CSC400'], 'CSC100': []};
 
 function Course(prereqs){
 
@@ -11,18 +13,21 @@ function Course(prereqs){
 function pushCourseWithFulfilledPrereq(coursesWithPrereq, order){
 	let fulfilled = 0;
 	for(let courseId in coursesWithPrereq) {
+		if(!order) break;
 		let courseNotAdded = !order.includes(courseId);
 		let coursePrereqsFulfilled = coursesWithPrereq[courseId].every((prereqId) => order.includes(prereqId));
 		if(courseNotAdded && coursePrereqsFulfilled){
 			order.push(courseId);
 			fulfilled ++;
 			if(Object.keys(coursesWithPrereq).length > order.length){
-				pushCourseWithFulfilledPrereq(coursesWithPrereq, order);
+				order = pushCourseWithFulfilledPrereq(coursesWithPrereq, order);
 			}
 		}
 	}
-	if(fulfilled > 0) return order;
-	 else return null;
+	if(fulfilled === 0) {
+		order = null;
+	}
+	return order;
 }
 
 function courseOrder(coursesWithPrereq) {
@@ -31,3 +36,5 @@ function courseOrder(coursesWithPrereq) {
 }
 
 assert.equal(courseOrder(courses).join(), ['CSC100', 'CSC200', 'CSC300'].join());
+assert.equal(courseOrder(wrong_courses_1), null);
+assert.equal(courseOrder(wrong_courses_2), null);
